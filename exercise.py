35baaa7f8a5f373a96b575247cd910cc60824ef0
Chyld Medford <chyld.medford@gmail.com>
2018-01-09 10:31:32 -0600

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,32 @@
+# problem 1
+# ------------------------------------------------------------------- #
+people = ['Bob Smith', 'Ken Jones', 'Alex Bradino']
+
+
+def sort_by_last_name(people, order):
+    # return full names sorted by last name in either ascending or descending order
+    # add doctests make sure it passes
+    pass
+
+
+# problem 2
+# ------------------------------------------------------------------- #
+names = ['James', 'Susan', 'Maggie']
+ages = [4, 9, 12]
+
+
+def create_dictionary_from_lists(names, ages):
+    # {'James':4, 'Susan':9, 'Maggie':12}
+    # add doctests make sure it passes
+    pass
+
+
+# problem 3
+# ------------------------------------------------------------------- #
+numbers = [5, 6, 7, 8, 9, 10, 11, 12]
+
+
+def square_even_values_and_sum_under_10(numbers):
+    # 6^2 + 8^2]
+    # add doctests make sure it passes
+    pass
