aa41064836bab5bfc0700ee234915421127d67c8
Halm <Melissa.Halm@mckesson.com>
2018-01-09 11:39:07 -0600

    Add answers

--- exercise.py
+++ exercise.py
@@ -6,8 +6,15 @@ people = ['Bob Smith', 'Ken Jones', 'Alex Bradino']
 def sort_by_last_name(people, order):
     # return full names sorted by last name in either ascending or descending order
     # add doctests make sure it passes
-    pass
+    """
+    arguments:
+    Order = True if you want to sort descending; False if you want to sort ascending
 
+    >>> sort_by_last_name(people, True)
+    ['Bob Smith', 'Ken Jones', 'Alex Bradino']
+    """
+
+    return sorted(people, key=lambda x: x.split()[1], reverse=order)
 
 # problem 2
 # ------------------------------------------------------------------- #
@@ -18,8 +25,11 @@ ages = [4, 9, 12]
 def create_dictionary_from_lists(names, ages):
     # {'James':4, 'Susan':9, 'Maggie':12}
     # add doctests make sure it passes
-    pass
-
+    """
+    >>> create_dictionary_from_lists(names, ages)
+    {'James': 4, 'Susan': 9, 'Maggie': 12}
+    """
+    return dict(zip(names,ages))
 
 # problem 3
 # ------------------------------------------------------------------- #
@@ -29,4 +39,9 @@ numbers = [5, 6, 7, 8, 9, 10, 11, 12]
 def square_even_values_and_sum_under_10(numbers):
     # 6^2 + 8^2]
     # add doctests make sure it passes
-    pass
+    """
+    >>> square_even_values_and_sum_under_10(numbers)
+    100
+    """
+    b=[n**2 for n in numbers if n%2==0 and n<10]
+    return sum(b)
