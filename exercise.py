6017e6b9e179caf4c78b989b3dbf030d2d897afb
Chudgar <Jennifer.Chudgar@McKesson.com>
2018-01-09 11:39:29 -0600

    done

--- exercise.py
+++ exercise.py
@@ -1,12 +1,23 @@
 # problem 1
 # ------------------------------------------------------------------- #
+"""
+python -m doctest -v exercise.py
+
+"""
+
 people = ['Bob Smith', 'Ken Jones', 'Alex Bradino']
 
+def sort_by_last_name(people):
+    """
+    sorts list alphabetically
+    >>> sort_by_last_name(people)
+    ['Alex Bradino', 'Ken Jones', 'Bob Smith']
 
-def sort_by_last_name(people, order):
-    # return full names sorted by last name in either ascending or descending order
+    """
+    # return full names sorted by last name in ascending order
+    # ['Alex Bradino', 'Ken Jones', 'Bob Smith']
     # add doctests make sure it passes
-    pass
+    return sorted(sorted(people), key=lambda n: n.split()[1])
 
 
 # problem 2
@@ -14,11 +25,17 @@ def sort_by_last_name(people, order):
 names = ['James', 'Susan', 'Maggie']
 ages = [4, 9, 12]
 
-
 def create_dictionary_from_lists(names, ages):
+    """
+    creates a new dictionary from lists names and ages
+    >>> create_dictionary_from_lists(names, ages)
+    {'James': 4, 'Maggie': 12, 'Susan': 9}
+    """
     # {'James':4, 'Susan':9, 'Maggie':12}
     # add doctests make sure it passes
-    pass
+    mydict = {}
+    for i in range(len(names)):
+        mydict[names[i]] = ages[i]
 
 
 # problem 3
@@ -27,6 +44,11 @@ numbers = [5, 6, 7, 8, 9, 10, 11, 12]
 
 
 def square_even_values_and_sum_under_10(numbers):
+    """
+    the squares for only the even values in list numbers that are less than 10
+    >>> square_even_values_and_sum_under_10(numbers)
+    100
+    """
     # 6^2 + 8^2]
     # add doctests make sure it passes
-    pass
+    return sum([n**2 for n in numbers if (n<10 and n%2==0)])
